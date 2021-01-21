6180a41506bffb1d2a3818355ca50546dbe5bc8b
victor <victoroliveira.eng@hotmail.com>
2021-01-20 21:39:42 -0300

    Add: custom_generator, standardization

---
+++ standardization.py
@@ -0,0 +1,14 @@
+class Standardize(keras.utils.Sequence):
+   
+   def __init__(self, img):
+      
+      self.img = img
+
+   def standardize(self):
+      img[img < 0] = 0
+      mask = img > 0 # bool
+      mean = np.mean(img[mask]) # mean
+      std = np.std(img[mask])   # std
+
+      img[mask] = (img[mask] - mean)/ std
+      return img
