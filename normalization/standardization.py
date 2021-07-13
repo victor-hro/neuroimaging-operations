1c0c525da788d5ea5cd06ba88c9d895075415aa6
victor <victoroliveira.eng@hotmail.com>
2021-07-13 16:52:57 -0300

    funçao standardize

--- normalization/standardization.py
+++ normalization/standardization.py
@@ -1,14 +1,8 @@
-class Standardize():
-   
-   def __init__(self, img):
-      
-      self.img = img
+def standardize(self):
+   img[img < 0] = 0
+   mask = img > 0 # bool
+   mean = np.mean(img[mask]) # mean
+   std = np.std(img[mask])   # std
 
-   def standardize(self):
-      img[img < 0] = 0
-      mask = img > 0 # bool
-      mean = np.mean(img[mask]) # mean
-      std = np.std(img[mask])   # std
-
-      img[mask] = (img[mask] - mean)/ std
-      return img
+   img[mask] = (img[mask] - mean)/ std
+   return img
